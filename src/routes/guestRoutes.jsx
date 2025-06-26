9d53486aa6bc33b9d6221e3bddfea19256cd689e
thangtdse171466@fpt.edu.vn <tranduongthang8583@gmail.com>
2025-06-26 16:44:59 +0700

    fix blogpage and add blogdetailpage

--- src/routes/guestRoutes.jsx
+++ src/routes/guestRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "../components/home/HomePage";
 import BlogPage from "../pages/blog/BlogPage";
+import BlogDetailPage from "../pages/blog/BlogDetailPage";
 
 const guestRoutes = (
   <>
     <Route path="/" element={<HomePage />} />
     <Route path="/blog" element={<BlogPage />} />
+    <Route path="/blog/:id" element={<BlogDetailPage />} />
   </>
 );
 
